392f4ea96e65422223f3518b0646311b89a84fa0
Sutrisno Efendi <kangfend@gmail.com>
2015-08-30 03:18:27 +0700

    Bump up version to 0.1.3

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 setup(
     name='py-jne',
     packages=['jne'],
-    version='0.1.2',
+    version='0.1.3',
     description='PyJNE is a tool to simplify the user in using the JNE API such as tracking stuff, check tariffs and others.',
     license='MIT',
     author='Sutrisno Efendi',
     author_email='kangfend@gmail.com',
     url='https://github.com/kangfend/py-jne',
-    download_url='https://github.com/kangfend/py-jne/tarball/0.1.2',
+    download_url='https://github.com/kangfend/py-jne/tarball/0.1.3',
     keywords=['JNE', 'Tracking', 'Courier', 'Indonesia'],
     install_requires=['requests==2.6.0'],
     classifiers=[
